67063b3da4c6babc96ca4b598c4d95f133d4cfbe
Masahiro Niitani <masahiro.niitani@gmail.com>
2023-04-05 20:00:52 +0900

    init

---
+++ ConsoleApp1/ConsoleApp1/Hira.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Hira : Shain
+    {
+        public void standup()
+        {
+            Console.WriteLine("平社員が立ちました。");
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using ConsoleApp1;
+
+Random rdm = new System.Random();
+int r = rdm.Next(0, 3);
+Console.WriteLine("r=" + r);
+
+Shain shain;
+
+if (r == 0)
+{
+    shain = new Hira();
+}
+else if (r == 1)
+{
+    shain = new Kacho();
+}
+else
+{
+    shain = new Bucho();
+}
+
+shain.standup();
