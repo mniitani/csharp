281403b16d8207da272121a33305cf86318dcd72
Masahiro Niitani <masahiro.niitani@gmail.com>
2023-04-05 21:02:31 +0900

    updated

---
+++ ConsoleApp2/ConsoleApp2/Bucho.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Bucho : Shain
+    {
+        public void standup()
+        {
+            Console.WriteLine("部長が立ちました。");
+        }
+    }
+}
